186c52001ad10cba5e0f4a0be6aaaaa64e0612c6
Zakharov Roman <zakharovroman@hotmail.com>
2020-05-31 12:33:25 +0300

    First storyboard

---
+++ HW2_4_Task_1/Models/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  HW2_4_Task_1
+//
+//  Created by Роман Захаров on 31.05.2020.
+//  Copyright © 2020 Роман Захаров. All rights reserved.
+//
+
+struct User {
+    let userName: String
+    let password: String
+}
